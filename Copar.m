f6cdde5b763c06301b8ba48c7705066acc8d5c3c
Naoya Yamashita <conao3@gmail.com>
2017-07-06 17:59:19 +0900

    create Copar

---
+++ Copar.m
@@ -0,0 +1,16 @@
+classdef Copar < handle
+    properties (GetAccess=public, SetAccess=private)
+        fileid = 0;
+    end
+    
+    methods
+        function obj = Copar()
+        end
+        
+        function openfile(filepath)
+        end
+        
+        function parse()
+        end
+    end
+end
