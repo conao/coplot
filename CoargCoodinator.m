15d427e7ef0a9056f4fd5ae9d7a2f6333cd26960
Naoya Yamashita <conao3@gmail.com>
2017-07-06 17:56:37 +0900

    create CoargCoodinator

---
+++ CoargCoodinator.m
@@ -0,0 +1,23 @@
+classdef CoargCoodinator < handle
+    properties (GetAccess=public, SetAccess=private)
+    end
+    methods
+        function obj = CoargCoodinator()
+        end
+        
+        function insertarg(target)
+        end
+        
+        function deletearg(key)
+        end
+        
+        function getarg(key)
+        end
+        
+        function appendValues(key, values)
+        end
+        
+        function keys = getkeys()
+        end
+    end
+end
