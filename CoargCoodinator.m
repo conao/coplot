2126f7ad10ccb466607893d37e8e409bfb5a41b7
Naoya Yamashita <conao3@gmail.com>
2017-07-07 10:15:19 +0900

    implement insertarg

--- CoargCoodinator.m
+++ CoargCoodinator.m
@@ -1,11 +1,18 @@
 classdef CoargCoodinator < handle
     properties (GetAccess=public, SetAccess=private)
+        coarg = []; % init with empty matlix
     end
     methods
         function obj = CoargCoodinator()
+            % no init code.
         end
         
-        function insertarg(target)
+        function insertarg(obj, target)
+            if isa(target, 'Coarg')
+                obj.coarg(length(obj.coarg)+1) = target;
+            else
+                error('insertarg need "Coarg" object');
+            end
         end
         
         function deletearg(key)
